312e7d382a083b3b6fa5de4afacdb3957b907b2d
nakayan <ayaka.nakatani@pocketpair.jp>
2019-11-26 00:38:44 +0900

    プログラミング少し書いた

---
+++ Tetris/Assets/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] groups;
+    {
+        public void spawnNext() {
+            //Rangesは、配列から呼び出す関数らしい
+            int i = Random.Range(0, groups.Length);
+            // Spawn Group at current Position
+            Instantiate(groups[i],
+            transform.position,
+            //回転を決めているらしい
+            Quaternion.identity);
+
+        }
+    }
+
+}
