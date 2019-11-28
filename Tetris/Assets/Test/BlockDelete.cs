64ea9d0ac89809f7c423b9f177a682fab26bb822
nakayan <ayaka.nakatani@pocketpair.jp>
2019-11-28 23:03:53 +0900

    配列とfor文の練習中

---
+++ Tetris/Assets/Test/BlockDelete.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockDelete : MonoBehaviour {
+public GameObject obj;
+
+public void Delete()
+    {
+        Destroy(obj);
+    }
+	
+}
