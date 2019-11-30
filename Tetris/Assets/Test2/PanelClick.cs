84266c9b4e5d42cab27a53f0bc24f9939ce8cd0f
ayakanakatani <flei.rainbow@gmail.com>
2019-11-30 18:44:18 +0900

    新しいシーン作った。とりまバグは直した。

---
+++ Tetris/Assets/Test2/PanelClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelClick : MonoBehaviour {
+
+public void Click()
+    {
+        Debug.Log(this.transform.position.x +"," + this.transform.position.y);
+        Test2.panelG[(int)this.transform.position.x, (int)this.transform.position.y] = 99;
+    }
+}
---
+++ Tetris/Assets/Test2/Test2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour {
+    static int panelMax = 5;
+    public GameObject [] panelPf;
+    public static int[,] panelG = new int[panelMax, panelMax];
+    public static int x;
+    public static int y;
+
+    // Use this for initialization
+    void Start () {
+        int[,] panelG = new int[panelMax, panelMax];
+        for (x = 0; x < panelMax; x++)
+            for (y = 0; y < panelMax; y++)
+            {
+                panelG[x, y] = randomNum(3);
+                Instantiate(panelPf[0], new Vector3(x, y, 0), Quaternion.identity);
+            }
+                
+                
+
+                /*
+
+                if (panelG[x, y] == 0)
+                {
+                    Instantiate(panelPf[0], new Vector3(x, y, 0), Quaternion.identity);
+                }
+                if (panelG[x, y] == 1)
+                {
+                    Instantiate(panelPf[1], new Vector3(x, y, 0), Quaternion.identity);
+                }
+                if (panelG[x, y] == 2)
+                {
+                    Instantiate(panelPf[2], new Vector3(x, y, 0), Quaternion.identity);
+                }
+                */
+            
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    int randomNum(int r)
+    {
+        return (Random.Range(0, r));
+    }
+}
