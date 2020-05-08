588f9f361dd2bfcf158caafd87b7d37cf5dee305
ayakanakatani <flei.rainbow@gmail.com>
2020-05-08 20:29:45 +0900

    一次元配列で登録したOBJを二次元配列に入れなおし成功！

---
+++ Tetris/Assets/Reverse/Script/PanelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelManager : MonoBehaviour
+{
+    public GameObject[] obj_Panels;
+    public GameObject[,] field_Panels = new GameObject[5, 5];
+    // Use this for initialization
+    void Start()
+    {
+        //パネルを二次元配列に入れなおし
+        int x, y;
+        int c = 0;
+        for (y = 0; y < 5; y++)
+            for (x = 0; x < 5; x++)
+            {
+                field_Panels[x, y] = obj_Panels[c];
+                c += 1;
+            }
+        Debug.Log(field_Panels[3, 3].gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
